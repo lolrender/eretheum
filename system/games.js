4e4fa1c3396e63832159ef44e258d5da73b5223f
cxon <145627982+lolrender@users.noreply.github.com>
2026-09-18 14:54:49 -0400

    Mark games with save support

--- system/games.js
+++ system/games.js
@@ -1,416 +1,71 @@
 const games = [
-  {
-    "title": "2048",
-    "id": "2048",
-    "tag": "Arcade",
-    "image": "projects/2048/thumb.png"
-  },
-  {
-    "title": "Achievement Unlocked",
-    "id": "achievementunlocked",
-    "tag": "Arcade",
-    "image": "projects/achievementunlocked/achievementunlocked.png"
-  },
-  {
-    "title": "Adrenaline Challenge",
-    "id": "adrenalinechallenge",
-    "tag": "Arcade",
-    "image": "projects/adrenalinechallenge/adrenalinechallenge.jpg"
-  },
-  {
-    "title": "Avalanche",
-    "id": "avalanche",
-    "tag": "Arcade",
-    "image": "projects/avalanche/avalanche.png"
-  },
-  {
-    "title": "Big Red Button",
-    "id": "bigredbutton",
-    "tag": "Arcade",
-    "image": "projects/bigredbutton/bigredbutton.png"
-  },
-  {
-    "title": "Bloxors",
-    "id": "bloxors",
-    "tag": "Puzzle",
-    "image": "projects/bloxors/block.png"
-  },
-  {
-    "title": "Big Tower Tiny Square",
-    "id": "btts",
-    "tag": "Platformer",
-    "image": "projects/btts/images.png"
-  },
-  {
-    "title": "Cell Machine",
-    "id": "cell-machine",
-    "tag": "Puzzle",
-    "image": "projects/cell-machine/img/te9nDu.png"
-  },
-  {
-    "title": "Chrome Dino",
-    "id": "chrome-dino",
-    "tag": "Arcade",
-    "image": "projects/chrome-dino/icons/icon-256.png"
-  },
-  {
-    "title": "circlO",
-    "id": "circlo",
-    "tag": "Puzzle",
-    "image": "projects/circlo/img/download.png"
-  },
-  {
-    "title": "Connect 3",
-    "id": "connect3",
-    "tag": "Puzzle",
-    "image": "projects/connect3/connect3.png"
-  },
-  {
-    "title": "Cookie Clicker",
-    "id": "cookie-clicker",
-    "tag": "Idle",
-    "image": "projects/cookie-clicker/cookie1.jpeg"
-  },
-  {
-    "title": "CraftMine",
-    "id": "craftmine",
-    "tag": "Sandbox",
-    "image": "projects/craftmine/images/craftmine.png"
-  },
-  {
-    "title": "Cut the Rope",
-    "id": "ctr",
-    "tag": "Puzzle",
-    "image": "projects/ctr/logo.png"
-  },
-  {
-    "title": "Cut the Rope Holiday",
-    "id": "ctr-holiday",
-    "tag": "Puzzle",
-    "image": "projects/ctr-holiday/Holiday_Gift.webp"
-  },
-  {
-    "title": "Cut the Rope Time Travel",
-    "id": "ctr-tr",
-    "tag": "Puzzle",
-    "image": "projects/ctr-tr/logo.png"
-  },
-  {
-    "title": "Cubefield",
-    "id": "cubefield",
-    "tag": "Arcade",
-    "image": "projects/cubefield/cubefield.jpg"
-  },
-  {
-    "title": "Cupcake 2048",
-    "id": "cupcake2048",
-    "tag": "Puzzle",
-    "image": "projects/cupcake2048/favicon.png"
-  },
-  {
-    "title": "Doge 2048",
-    "id": "doge2048",
-    "tag": "Puzzle",
-    "image": "projects/doge2048/meta/apple-touch-icon.png"
-  },
-  {
-    "title": "Doge Miner",
-    "id": "dogeminer",
-    "tag": "Idle",
-    "image": "projects/DogeMiner/img/dogeminer_300x300.png"
-  },
-  {
-    "title": "Doodle Jump",
-    "id": "doodle-jump",
-    "tag": "Arcade",
-    "image": "projects/doodle-jump/doodle.png"
-  },
-  {
-    "title": "Double Wires",
-    "id": "doublewires",
-    "tag": "Arcade",
-    "image": "projects/doublewires/doublewires.png"
-  },
-  {
-    "title": "Draw the Hill",
-    "id": "draw-the-hill",
-    "tag": "Arcade",
-    "image": "projects/draw-the-hill/icons/icon-512.png"
-  },
-  {
-    "title": "Duck Life",
-    "id": "ducklife1",
-    "tag": "Sports",
-    "image": "projects/ducklife1/ducklife.png"
-  },
-  {
-    "title": "Duck Life 2",
-    "id": "ducklife2",
-    "tag": "Sports",
-    "image": "projects/ducklife2/ducklife2.png"
-  },
-  {
-    "title": "Duck Life 3",
-    "id": "ducklife3",
-    "tag": "Sports",
-    "image": "projects/ducklife3/duck.png"
-  },
-  {
-    "title": "Elastic Man",
-    "id": "elasticman",
-    "tag": "Arcade",
-    "image": "projects/elasticman/elasticman.jpg"
-  },
-  {
-    "title": "Factory Balls Forever",
-    "id": "factoryballs",
-    "tag": "Puzzle",
-    "image": "projects/factoryballs/images/splash.png"
-  },
-  {
-    "title": "Flappy 2048",
-    "id": "flappy-2048",
-    "tag": "Arcade",
-    "image": "projects/flappy-2048/meta/apple-touch-icon.png"
-  },
-  {
-    "title": "Flappy Bird",
-    "id": "flappy-bird",
-    "tag": "Arcade",
-    "image": "projects/flappy-bird/assets/thumb.png"
-  },
-  {
-    "title": "Flash Tetris",
-    "id": "flashtetris",
-    "tag": "Puzzle",
-    "image": "projects/flashtetris/flashtetris.png"
-  },
-  {
-    "title": "Game Inside a Game Inside a Game...",
-    "id": "game-inside",
-    "tag": "Puzzle",
-    "image": "projects/game-inside/img/display.png"
-  },
-  {
-    "title": "Geometry Dash (Scratch)",
-    "id": "geodash",
-    "tag": "Arcade",
-    "image": "projects/geodash/geoscratchicon.png"
-  },
-  {
-    "title": "Gimme the Airpod",
-    "id": "gimme-the-airpod",
-    "tag": "Arcade",
-    "image": "projects/gimme-the-airpod/img/logo.png"
-  },
-  {
-    "title": "Glass City",
-    "id": "glass-city",
-    "tag": "Arcade",
-    "image": "projects/glass-city/image.png"
-  },
-  {
-    "title": "Google Snake",
-    "id": "google-snake",
-    "tag": "Arcade",
-    "image": "projects/google-snake/img/snake.png"
-  },
-  {
-    "title": "Grind Craft",
-    "id": "grindcraft",
-    "tag": "Idle",
-    "image": "projects/grindcraft/img/splash.png"
-  },
-  {
-    "title": "Hacker Typer",
-    "id": "hackertype",
-    "tag": "Arcade",
-    "image": "projects/hackertype/logo192.png"
-  },
-  {
-    "title": "Hextris",
-    "id": "hextris",
-    "tag": "Puzzle",
-    "image": "projects/hextris/images/hextris-logo.png"
-  },
-  {
-    "title": "Idle Breakout",
-    "id": "idle-breakout",
-    "tag": "Idle",
-    "image": "projects/idle-breakout/img/thumbnail.png"
-  },
-  {
-    "title": "Impossible Quiz",
-    "id": "impossiblequiz",
-    "tag": "Puzzle",
-    "image": "projects/impossiblequiz/impossiblequiz.png"
-  },
-  {
-    "title": "Interactive Buddy",
-    "id": "interactivebuddy",
-    "tag": "Arcade",
-    "image": "projects/interactivebuddy/interactivebuddy.jpg"
-  },
-  {
-    "title": "Learn To Fly",
-    "id": "learntofly",
-    "tag": "Arcade",
-    "image": "projects/learntofly/learntofly.png"
-  },
-  {
-    "title": "Minecraft Classic",
-    "id": "minecraft-classic",
-    "tag": "Sandbox",
-    "image": "projects/minecraft-classic/pack.png"
-  },
-  {
-    "title": "Minesweeper",
-    "id": "minesweeper",
-    "tag": "Puzzle",
-    "image": "projects/minesweeper/img/minesweeper.png"
-  },
-  {
-    "title": "Mini-putt",
-    "id": "miniputt",
-    "tag": "Sports",
-    "image": "projects/miniputt/miniputt.png"
-  },
-  {
-    "title": "Papery Planes",
-    "id": "papery-planes",
-    "tag": "Arcade",
-    "image": "projects/papery-planes/splash.jpg"
-  },
-  {
-    "title": "Particle Clicker",
-    "id": "particle-clicker",
-    "tag": "Idle",
-    "image": "projects/particle-clicker/assets/mobile/original.png"
-  },
-  {
-    "title": "PolyBranch",
-    "id": "polybranch",
-    "tag": "Arcade",
-    "image": "projects/polybranch/img/pic1.png"
-  },
-  {
-    "title": "Push The Square",
-    "id": "push-the-square",
-    "tag": "Puzzle",
-    "image": "projects/push-the-square/img/splash.png"
-  },
-  {
-    "title": "Retro Bowl",
-    "id": "retro-bowl",
-    "tag": "Sports",
-    "image": "projects/retro-bowl/img/icon.jpg"
-  },
-  {
-    "title": "Rolly Vortex",
-    "id": "rolly-vortex",
-    "tag": "Arcade",
-    "image": "projects/rolly-vortex/icon-256.png"
-  },
-  {
-    "title": "Sand Game",
-    "id": "sand-game",
-    "tag": "Sandbox",
-    "image": "projects/sand-game/sand.png"
-  },
-  {
-    "title": "Slope",
-    "id": "slope",
-    "tag": "Arcade",
-    "image": "projects/slope/slope4.jpeg"
-  },
-  {
-    "title": "Solitaire",
-    "id": "solitaire",
-    "tag": "Puzzle",
-    "image": "projects/solitaire/screen-shot.png"
-  },
-  {
-    "title": "Sort The Court",
-    "id": "sort-the-court",
-    "tag": "Strategy",
-    "image": "projects/sort-the-court/img/splash.png"
-  },
-  {
-    "title": "Soundboard",
-    "id": "soundboard",
-    "tag": "Arcade",
-    "image": "projects/soundboard/img/mlg-favicon.png"
-  },
-  {
-    "title": "Stack",
-    "id": "stack",
-    "tag": "Arcade",
-    "image": "projects/stack/stack.png"
-  },
-  {
-    "title": "Stack Bump 3D",
-    "id": "stack-bump-3d",
-    "tag": "Arcade",
-    "image": "projects/stack-bump-3d/thumbnail.jpg"
-  },
-  {
-    "title": "Swerve",
-    "id": "swerve",
-    "tag": "Arcade",
-    "image": "projects/swerve/img/favicon.jpg"
-  },
-  {
-    "title": "Tanuki Sunset",
-    "id": "tanuki-sunset",
-    "tag": "Arcade",
-    "image": "projects/tanuki-sunset/img/cover.png"
-  },
-  {
-    "title": "Temple Run 2",
-    "id": "temple-run-2",
-    "tag": "Arcade",
-    "image": "projects/temple-run-2/img/og-icon.png"
-  },
-  {
-    "title": "The Final Earth",
-    "id": "the-final-earth",
-    "tag": "Strategy",
-    "image": "projects/the-final-earth/images.png"
-  },
-  {
-    "title": "There Is No Game",
-    "id": "there-is-no-game",
-    "tag": "Puzzle",
-    "image": "projects/there-is-no-game/logo.png"
-  },
-  {
-    "title": "This Is The Only Level",
-    "id": "thisistheonlylevel",
-    "tag": "Platformer",
-    "image": "projects/thisistheonlylevel/thisistheonlylevel.png"
-  },
-  {
-    "title": "Tunnel Rush",
-    "id": "tunnel-rush",
-    "tag": "Arcade",
-    "image": "projects/tunnel-rush/img/tunnel.jpg"
-  },
-  {
-    "title": "Wordle",
-    "id": "wordle",
-    "tag": "Puzzle",
-    "image": "projects/wordle/img/logo_512x512.png"
-  },
-  {
-    "title": "World's Hardest Game",
-    "id": "worlds-hardest-game",
-    "tag": "Arcade",
-    "image": "projects/worlds-hardest-game/images/splash.jpg"
-  },
-  {
-    "title": "World's Hardest Game 2",
-    "id": "worlds-hardest-game-2",
-    "tag": "Arcade",
-    "image": "projects/worlds-hardest-game-2/the-worlds-hardest-game-2.jpg"
-  }
+  {"title":"2048","id":"2048","tag":"Arcade","image":"projects/2048/thumb.png","save":true},
+  {"title":"Achievement Unlocked","id":"achievementunlocked","tag":"Arcade","image":"projects/achievementunlocked/achievementunlocked.png","save":false},
+  {"title":"Adrenaline Challenge","id":"adrenalinechallenge","tag":"Arcade","image":"projects/adrenalinechallenge/adrenalinechallenge.jpg","save":false},
+  {"title":"Avalanche","id":"avalanche","tag":"Arcade","image":"projects/avalanche/avalanche.png","save":false},
+  {"title":"Big Red Button","id":"bigredbutton","tag":"Arcade","image":"projects/bigredbutton/bigredbutton.png","save":false},
+  {"title":"Bloxors","id":"bloxors","tag":"Puzzle","image":"projects/bloxors/block.png","save":false},
+  {"title":"Big Tower Tiny Square","id":"btts","tag":"Platformer","image":"projects/btts/images.png","save":true},
+  {"title":"Cell Machine","id":"cell-machine","tag":"Puzzle","image":"projects/cell-machine/img/te9nDu.png","save":false},
+  {"title":"Chrome Dino","id":"chrome-dino","tag":"Arcade","image":"projects/chrome-dino/icons/icon-256.png","save":false},
+  {"title":"circlO","id":"circlo","tag":"Puzzle","image":"projects/circlo/img/download.png","save":false},
+  {"title":"Connect 3","id":"connect3","tag":"Puzzle","image":"projects/connect3/connect3.png","save":false},
+  {"title":"Cookie Clicker","id":"cookie-clicker","tag":"Idle","image":"projects/cookie-clicker/cookie1.jpeg","save":true},
+  {"title":"CraftMine","id":"craftmine","tag":"Sandbox","image":"projects/craftmine/images/craftmine.png","save":true},
+  {"title":"Cut the Rope","id":"ctr","tag":"Puzzle","image":"projects/ctr/logo.png","save":true},
+  {"title":"Cut the Rope Holiday","id":"ctr-holiday","tag":"Puzzle","image":"projects/ctr-holiday/Holiday_Gift.webp","save":true},
+  {"title":"Cut the Rope Time Travel","id":"ctr-tr","tag":"Puzzle","image":"projects/ctr-tr/logo.png","save":true},
+  {"title":"Cubefield","id":"cubefield","tag":"Arcade","image":"projects/cubefield/cubefield.jpg","save":false},
+  {"title":"Cupcake 2048","id":"cupcake2048","tag":"Puzzle","image":"projects/cupcake2048/favicon.png","save":true},
+  {"title":"Doge 2048","id":"doge2048","tag":"Puzzle","image":"projects/doge2048/meta/apple-touch-icon.png","save":true},
+  {"title":"Doge Miner","id":"dogeminer","tag":"Idle","image":"projects/DogeMiner/img/dogeminer_300x300.png","save":true},
+  {"title":"Doodle Jump","id":"doodle-jump","tag":"Arcade","image":"projects/doodle-jump/doodle.png","save":false},
+  {"title":"Double Wires","id":"doublewires","tag":"Arcade","image":"projects/doublewires/doublewires.png","save":false},
+  {"title":"Draw the Hill","id":"draw-the-hill","tag":"Arcade","image":"projects/draw-the-hill/icons/icon-512.png","save":true},
+  {"title":"Duck Life","id":"ducklife1","tag":"Sports","image":"projects/ducklife1/ducklife.png","save":true},
+  {"title":"Duck Life 2","id":"ducklife2","tag":"Sports","image":"projects/ducklife2/ducklife2.png","save":true},
+  {"title":"Duck Life 3","id":"ducklife3","tag":"Sports","image":"projects/ducklife3/duck.png","save":true},
+  {"title":"Elastic Man","id":"elasticman","tag":"Arcade","image":"projects/elasticman/elasticman.jpg","save":false},
+  {"title":"Factory Balls Forever","id":"factoryballs","tag":"Puzzle","image":"projects/factoryballs/images/splash.png","save":true},
+  {"title":"Flappy 2048","id":"flappy-2048","tag":"Arcade","image":"projects/flappy-2048/meta/apple-touch-icon.png","save":false},
+  {"title":"Flappy Bird","id":"flappy-bird","tag":"Arcade","image":"projects/flappy-bird/assets/thumb.png","save":false},
+  {"title":"Flash Tetris","id":"flashtetris","tag":"Puzzle","image":"projects/flashtetris/flashtetris.png","save":true},
+  {"title":"Game Inside a Game Inside a Game...","id":"game-inside","tag":"Puzzle","image":"projects/game-inside/img/display.png","save":false},
+  {"title":"Geometry Dash (Scratch)","id":"geodash","tag":"Arcade","image":"projects/geodash/geoscratchicon.png","save":true},
+  {"title":"Gimme the Airpod","id":"gimme-the-airpod","tag":"Arcade","image":"projects/gimme-the-airpod/img/logo.png","save":false},
+  {"title":"Glass City","id":"glass-city","tag":"Arcade","image":"projects/glass-city/image.png","save":false},
+  {"title":"Google Snake","id":"google-snake","tag":"Arcade","image":"projects/google-snake/img/snake.png","save":false},
+  {"title":"Grind Craft","id":"grindcraft","tag":"Idle","image":"projects/grindcraft/img/splash.png","save":true},
+  {"title":"Hacker Typer","id":"hackertype","tag":"Arcade","image":"projects/hackertype/logo192.png","save":false},
+  {"title":"Hextris","id":"hextris","tag":"Puzzle","image":"projects/hextris/images/hextris-logo.png","save":false},
+  {"title":"Idle Breakout","id":"idle-breakout","tag":"Idle","image":"projects/idle-breakout/img/thumbnail.png","save":true},
+  {"title":"Impossible Quiz","id":"impossiblequiz","tag":"Puzzle","image":"projects/impossiblequiz/impossiblequiz.png","save":false},
+  {"title":"Interactive Buddy","id":"interactivebuddy","tag":"Arcade","image":"projects/interactivebuddy/interactivebuddy.jpg","save":false},
+  {"title":"Learn To Fly","id":"learntofly","tag":"Arcade","image":"projects/learntofly/learntofly.png","save":true},
+  {"title":"Minecraft Classic","id":"minecraft-classic","tag":"Sandbox","image":"projects/minecraft-classic/pack.png","save":false},
+  {"title":"Minesweeper","id":"minesweeper","tag":"Puzzle","image":"projects/minesweeper/img/minesweeper.png","save":false},
+  {"title":"Mini-putt","id":"miniputt","tag":"Sports","image":"projects/miniputt/miniputt.png","save":false},
+  {"title":"Papery Planes","id":"papery-planes","tag":"Arcade","image":"projects/papery-planes/splash.jpg","save":false},
+  {"title":"Particle Clicker","id":"particle-clicker","tag":"Idle","image":"projects/particle-clicker/assets/mobile/original.png","save":true},
+  {"title":"PolyBranch","id":"polybranch","tag":"Arcade","image":"projects/polybranch/img/pic1.png","save":false},
+  {"title":"Push The Square","id":"push-the-square","tag":"Puzzle","image":"projects/push-the-square/img/splash.png","save":false},
+  {"title":"Retro Bowl","id":"retro-bowl","tag":"Sports","image":"projects/retro-bowl/img/icon.jpg","save":true},
+  {"title":"Rolly Vortex","id":"rolly-vortex","tag":"Arcade","image":"projects/rolly-vortex/icon-256.png","save":false},
+  {"title":"Sand Game","id":"sand-game","tag":"Sandbox","image":"projects/sand-game/sand.png","save":true},
+  {"title":"Slope","id":"slope","tag":"Arcade","image":"projects/slope/slope4.jpeg","save":false},
+  {"title":"Solitaire","id":"solitaire","tag":"Puzzle","image":"projects/solitaire/screen-shot.png","save":true},
+  {"title":"Sort The Court","id":"sort-the-court","tag":"Strategy","image":"projects/sort-the-court/img/splash.png","save":true},
+  {"title":"Soundboard","id":"soundboard","tag":"Arcade","image":"projects/soundboard/img/mlg-favicon.png","save":false},
+  {"title":"Stack","id":"stack","tag":"Arcade","image":"projects/stack/stack.png","save":false},
+  {"title":"Stack Bump 3D","id":"stack-bump-3d","tag":"Arcade","image":"projects/stack-bump-3d/thumbnail.jpg","save":false},
+  {"title":"Swerve","id":"swerve","tag":"Arcade","image":"projects/swerve/img/favicon.jpg","save":false},
+  {"title":"Tanuki Sunset","id":"tanuki-sunset","tag":"Arcade","image":"projects/tanuki-sunset/img/cover.png","save":false},
+  {"title":"Temple Run 2","id":"temple-run-2","tag":"Arcade","image":"projects/temple-run-2/img/og-icon.png","save":true},
+  {"title":"The Final Earth","id":"the-final-earth","tag":"Strategy","image":"projects/the-final-earth/images.png","save":true},
+  {"title":"There Is No Game","id":"there-is-no-game","tag":"Puzzle","image":"projects/there-is-no-game/logo.png","save":false},
+  {"title":"This Is The Only Level","id":"thisistheonlylevel","tag":"Platformer","image":"projects/thisistheonlylevel/thisistheonlylevel.png","save":false},
+  {"title":"Tunnel Rush","id":"tunnel-rush","tag":"Arcade","image":"projects/tunnel-rush/img/tunnel.jpg","save":false},
+  {"title":"Wordle","id":"wordle","tag":"Puzzle","image":"projects/wordle/img/logo_512x512.png","save":true},
+  {"title":"World's Hardest Game","id":"worlds-hardest-game","tag":"Arcade","image":"projects/worlds-hardest-game/images/splash.jpg","save":false},
+  {"title":"World's Hardest Game 2","id":"worlds-hardest-game-2","tag":"Arcade","image":"projects/worlds-hardest-game-2/the-worlds-hardest-game-2.jpg","save":false}
 ];
